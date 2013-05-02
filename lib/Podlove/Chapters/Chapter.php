804ad6e51fc0d2807c8e9525e537c1fdc766d01c
Eric Teubert <ericteubert@googlemail.com>
2013-05-02 13:18:57 +0200

    basic mp4chaps printer

--- lib/Podlove/Chapters/Chapter.php
+++ lib/Podlove/Chapters/Chapter.php
@@ -13,4 +13,26 @@ class Chapter {
 		$this->link = $link;
 	}
 
+	public function get_title() {
+		return $this->title;
+	}
+
+	public function get_link() {
+		return $this->link;
+	}
+
+	public function get_time() {
+		$ms = $this->timestamp % 1000;
+		$s  = ($this->timestamp / 1000) % 60;
+		$m  = ($this->timestamp / 1000 / 60 ) % 60;
+		$h  = floor($this->timestamp / 1000 / 60 / 60);
+
+		while ( strlen( $h )  < 2 ) $h  = "0$h";
+		while ( strlen( $m )  < 2 ) $m  = "0$m";
+		while ( strlen( $s )  < 2 ) $s  = "0$s";
+		while ( strlen( $ms ) < 3 ) $ms = "0$ms";
+
+		return "$h:$m:$s.$ms";
+	}
+
 }
